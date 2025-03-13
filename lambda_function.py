cb130c375cf54e6d9401e400f2b1747c3d21d065
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 21:01:06 +0530

    Add Lambda function to terminate EC2 instance

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,13 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Create EC2 instance
-    ec2 = boto3.resource('ec2', region_name='us-east-1')
-    instance = ec2.create_instances(
-        ImageId='ami-08b5b3a93ed654d19',
-        InstanceType='t3.micro',
-        MinCount=1,
-        MaxCount=1
-    )
+    # Replace 'your_instance_id' with the actual instance ID
+    instance_id = 'your_instance_id'
     
-    return {
-        'instance_id': instance[0].id
-    }
+    ec2 = boto3.resource('ec2', region_name='us-east-1')
+    
+    # Terminate the EC2 instance
+    instance = ec2.Instance(instance_id)
+    response = instance.terminate()
+    
+    return response
