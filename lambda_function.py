0ead73fe1d516d3e79c9d288728fba17e7eb7e0a
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 20:41:02 +0530

    Create script to launch EC2 instance in us-east-1

--- lambda_function.py
+++ lambda_function.py
@@ -1,39 +1,19 @@
 import boto3
 
 def create_ec2_instance():
-    # Create a session using default credentials
-    session = boto3.Session(region_name='us-east-1')
-
-    # Create an EC2 client
-    ec2_client = session.client('ec2')
-
-    # Retrieve the latest Amazon Linux 2023 AMI ID
-    ami_response = ec2_client.describe_images(
-        Filters=[
-            {'Name': 'name', 'Values': ['*amzn2-ami-kernel-5.10-hvm-*']},
-            {'Name': 'owner-alias', 'Values': ['amazon']},
-            {'Name': 'state', 'Values': ['available']}
-        ],
-        Owners=['137112412989'],  # Amazon's official account ID
-        MaxResults=1,
-        SortBy='CreationDate',
-        SortOrder='Descending'
-    )
+    ec2_client = boto3.client('ec2', region_name='us-east-1')
     
-    latest_ami_id = ami_response['Images'][0]['ImageId']
-
-    # Launch an EC2 instance
-    instance_response = ec2_client.run_instances(
-        ImageId=latest_ami_id,
-        InstanceType='t3.micro',
-        KeyName='your-key-pair-name',  # Replace with your key pair name
+    instances = ec2_client.run_instances(
+        ImageId='ami-08b5b3a93ed654d19',
         MinCount=1,
         MaxCount=1,
-        SecurityGroupIds=['your-security-group-id'],  # Replace with your security group ID
+        InstanceType='t3.micro',
+        KeyName='your-key-pair-name',
+        SecurityGroupIds=[
+            'your-security-group-id',
+        ],
     )
+    print("EC2 Instance Created with ID:", instances['Instances'][0]['InstanceId'])
 
-    instance_id = instance_response['Instances'][0]['InstanceId']
-    print(f'EC2 Instance {instance_id} created with AMI {latest_ami_id}')
-
-# Call the function to create the instance
-create_ec2_instance()
+if __name__ == "__main__":
+    create_ec2_instance()
