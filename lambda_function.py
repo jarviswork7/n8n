9331e307f3be6ecb5cc8bb3bdf874c3d07aeba11
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 21:12:46 +0530

    Added Lambda function to create an EC2 instance in us-east-1.

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,15 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Specify the instance ID
-    instance_id = 'i-05d5b511aa662723a'
-    
+    # Create EC2 instance
     ec2 = boto3.resource('ec2', region_name='us-east-1')
+    instance = ec2.create_instances(
+        ImageId='ami-08b5b3a93ed654d19',
+        InstanceType='t3.micro',
+        MinCount=1,
+        MaxCount=1
+    )
     
-    # Terminate the EC2 instance
-    instance = ec2.Instance(instance_id)
-    response = instance.terminate()
-    
-    return response
+    return {
+        'instance_id': instance[0].id
+    }
