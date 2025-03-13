d7b0e03ab68f115d416e9e58b337ba26167b8287
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 16:38:16 +0530

    Add Lambda function code to list S3 buckets

--- code/test.py
+++ code/test.py
@@ -1,5 +1,1 @@
-def hello_world():
-    print('Hello, World!')
-
-if __name__ == '__main__':
-    hello_world()
+import boto3\n\ndef lambda_handler(event, context):\n    s3_client = boto3.client('s3')\n    response = s3_client.list_buckets()\n    bucket_names = [bucket['Name'] for bucket in response['Buckets']]\n    return {\n        'statusCode': 200,\n        'body': bucket_names\n    }
