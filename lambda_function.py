8359ce1b726803054e16da0e5df4c266c99289c5
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 20:05:09 +0530

    Add Python Lambda function to delete an S3 bucket

--- lambda_function.py
+++ lambda_function.py
@@ -1,28 +1,30 @@
-import json
 import boto3
-
-
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-    bucket_name = "n8n-tesast-98698798"
+    bucket_name = 'n8n-tesast-98698798'
+
+    # Create a session with boto3
     s3 = boto3.client('s3')
-    
-    try:
-        # Delete all objects in the bucket
-        objects = s3.list_objects_v2(Bucket=bucket_name)
-        if 'Contents' in objects:
-            for obj in objects['Contents']:
-                s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
-            
-        # Delete the bucket itself
-        s3.delete_bucket(Bucket=bucket_name)
-        
-        return {
-            'statusCode': 200,
-            'body': json.dumps(f'Successfully deleted bucket: {bucket_name}')
-        }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f'Error deleting bucket: {str(e)}')
-        }
+
+    # Function to delete an S3 bucket
+    def delete_s3_bucket(bucket_name):
+        try:
+            # Delete all objects in the bucket
+            objects = s3.list_objects_v2(Bucket=bucket_name)
+            if 'Contents' in objects:
+                for obj in objects['Contents']:
+                    s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+
+            # Now delete the bucket itself
+            s3.delete_bucket(Bucket=bucket_name)
+            return f'Bucket {bucket_name} deleted successfully.'
+        except ClientError as e:
+            return f'Error: {str(e)}'
+
+    # Call the function to delete the bucket
+    result = delete_s3_bucket(bucket_name)
+    return {
+        'statusCode': 200,
+        'body': result
+    }
