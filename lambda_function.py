caddce35ab2d061855886234ea1e81fbcb5e8690
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 19:53:06 +0530

    Add Lambda function to create EC2 instance with t3.micro

--- lambda_function.py
+++ lambda_function.py
@@ -4,10 +4,10 @@ import boto3
 def lambda_handler(event, context):
     ec2 = boto3.resource('ec2')
     instance = ec2.create_instances(
-        ImageId=event['ami_id'],
+        ImageId='ami-08b5b3a93ed654d19',
         MinCount=1,
         MaxCount=1,
-        InstanceType=event['instance_type']
+        InstanceType='t3.micro'
     )
     return {
         'statusCode': 200,
