664dc4ef62d82357f4f9e982e20252796d635bfb
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 21:47:20 +0530

    Add Lambda function to create an SQS named test-sqs

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,12 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Create EC2 instance
-    ec2 = boto3.resource('ec2', region_name='us-east-1')
-    instance = ec2.create_instances(
-        ImageId='ami-08b5b3a93ed654d19',
-        InstanceType='t3.micro',
-        MinCount=1,
-        MaxCount=1
-    )
+    # Create SQS client
+    sqs = boto3.client('sqs', region_name='us-east-1')
+    
+    # Create SQS queue
+    response = sqs.create_queue(QueueName='test-sqs')
     
     return {
-        'instance_id': instance[0].id
+        'QueueUrl': response['QueueUrl']
     }
