4aafff9841abe296169124e705972bbbe502e243
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 20:59:24 +0530

    Create a Lambda function to launch an EC2 instance in us-east-1

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,1 @@
-import boto3
-
-def lambda_handler(event, context):
-    # Create an EC2 client
-    ec2 = boto3.client('ec2', region_name='us-east-1')
-
-    # AMI ID for Amazon Linux 2023
-    ami_id = 'ami-08b5b3a93ed654d19'
-
-    # Launch a new EC2 instance with default parameters
-    response = ec2.run_instances(
-        ImageId=ami_id,
-        InstanceType='t3.micro',
-        MinCount=1,
-        MaxCount=1,
-    )
-
-    return response
+import boto3\n\ndef lambda_handler(event, context):\n    # Create EC2 instance\n    ec2 = boto3.resource('ec2', region_name='us-east-1')\n    instance = ec2.create_instances(\n        ImageId='ami-08b5b3a93ed654d19',\n        InstanceType='t3.micro',\n        MinCount=1,\n        MaxCount=1\n    )\n    \n    return {\n        'instance_id': instance[0].id\n    }
