d18b2c27b8387e26733a60503454a0ef8fa74d48
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 19:56:52 +0530

    Add Python Lambda function to create an S3 bucket

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,22 @@
-import boto3  
+import json
+import boto3
 
-def lambda_handler(event, context):  
-    ec2 = boto3.resource('ec2')  
-    # Create a new EC2 instance  
-    instance = ec2.create_instances(  
-        ImageId='ami-08b5b3a93ed654d19',  
-        MinCount=1,  
-        MaxCount=1,  
-        InstanceType='t3.micro'  
-    )  
-    print(f'New instance created: {instance[0].id}')  
-    return {  
-        'statusCode': 200,  
-        'body': f'Created instance with ID: {instance[0].id}'  
-    }
+def lambda_handler(event, context):
+    # Create an S3 client
+    s3 = boto3.client('s3')
+
+    # Bucket name
+    bucket_name = 'n8n-test-98698798'
+
+    # Create the S3 bucket
+    try:
+        response = s3.create_bucket(Bucket=bucket_name)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f'Bucket {bucket_name} created successfully!')
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error creating bucket: {str(e)}')
+        }
