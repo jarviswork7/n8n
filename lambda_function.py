bd10b0f7cfab137eceedf2b755106f4b98c3e207
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 18:20:41 +0530

    Add Lambda function to create an EC2 instance with t3.micro and specified AMI ID

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@ import boto3
 def lambda_handler(event, context):
     ec2 = boto3.resource('ec2')
     instance = ec2.create_instances(
+        ImageId='ami-08b5b3a93ed654d19',  # Specified AMI ID
         InstanceType='t3.micro',
         MinCount=1,
         MaxCount=1
