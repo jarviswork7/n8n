2cb8199cbeb5341ac684c071cfe4291de7d815cc
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-13 17:59:17 +0530

    Add Lambda function to list SQS queues

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,10 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2_client = boto3.client('ec2')
-    response = ec2_client.describe_instances()
-    instances = []
-    for reservation in response['Reservations']:
-        for instance in reservation['Instances']:
-            instances.append(instance['InstanceId'])
+    sqs_client = boto3.client('sqs')
+    response = sqs_client.list_queues()
+    queues = response.get('QueueUrls', [])
     return {
         'statusCode': 200,
-        'body': instances
+        'body': queues
     }
