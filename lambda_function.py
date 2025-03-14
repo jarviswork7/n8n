914e7a5b1e1584c48442c3269958491436135ab5
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 16:43:12 +0530

    Added code to delete S3 bucket named 'lambda-code-n8n'

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,17 @@
 import boto3
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3', region_name='us-east-1')
+    s3 = boto3.client('s3')
+    bucket_name = 'lambda-code-n8n'
     
-    response = s3.list_buckets()
-    buckets = [bucket['Name'] for bucket in response['Buckets']]
-    
-    return buckets
+    try:
+        # Delete all objects in the bucket
+        objects = s3.list_objects_v2(Bucket=bucket_name).get('Contents', [])
+        for obj in objects:
+            s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+        
+        # Delete the bucket
+        s3.delete_bucket(Bucket=bucket_name)
+        return {'status': 'Bucket deleted successfully'}
+    except Exception as e:
+        return {'error': str(e)}
