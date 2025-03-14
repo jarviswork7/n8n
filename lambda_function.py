25b2683376a146556c38e54e97d3a6643229313c
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 10:21:48 +0530

    Remove a specific tag from the EC2 instance i-08b5ba99795323b9a in us-east-1 region

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,14 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2_client = boto3.client('ec2', region_name='us-east-1')
+    ec2 = boto3.client('ec2', region_name='us-east-1')
     
-    try:
-        response = ec2_client.create_tags(
-            Resources=['i-08b5ba99795323b9a'],
-            Tags=[{'Key': 'Name', 'Value': 'n8n-instance'}]
-        )
-        return {'status': 'Tagging successful', 'response': response}
-    except Exception as e:
-        return {'status': 'Tagging failed', 'error': str(e)}
+    instance_id = 'i-08b5ba99795323b9a'
+    tag_key = 'Name'
+
+    ec2.delete_tags(Resources=[instance_id], Tags=[{'Key': tag_key}])
+    
+    return {
+        'statusCode': 200,
+        'body': f'Tag {tag_key} removed from instance {instance_id}'
+    }
