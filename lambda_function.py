a6ea984150258e5c4c8144613ab7c2ab01eeffe5
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 18:12:22 +0530

    Added Lambda function to create EC2 instance using best practices

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,16 @@
 import boto3
 
 def lambda_handler(event, context):
-    client = boto3.client('rds', region_name='us-east-1')
-    response = client.describe_db_instances(DBInstanceIdentifier='n8n')
-    
-    engine_version = response['DBInstances'][0]['EngineVersion']
-    
+    ec2 = boto3.resource('ec2', region_name='us-east-1')
+
+    instance = ec2.create_instances(
+        ImageId='ami-08b5b3a93ed654d19',  # Preferred AMI ID
+        InstanceType='t3.micro',           # Preferred instance type
+        MinCount=1,
+        MaxCount=1
+    )
+
     return {
-        'EngineVersion': engine_version
+        'InstanceId': instance[0].id,
+        'Status': 'EC2 instance created successfully'
     }
