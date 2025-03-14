77219b01987ba4240384571e3323402e8e9c2c17
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 17:09:43 +0530

    Add Lambda function to fetch RDS engine version for instance 'n8n'

--- lambda_function.py
+++ lambda_function.py
@@ -1,23 +1,11 @@
 import boto3
 
 def lambda_handler(event, context):
-    rds_client = boto3.client('rds', region_name='us-east-1')
+    client = boto3.client('rds', region_name='us-east-1')
+    response = client.describe_db_instances(DBInstanceIdentifier='n8n')
     
-    try:
-        response = rds_client.describe_db_instances()
-        db_instances = response['DBInstances']
-        
-        for db_instance in db_instances:
-            if db_instance['DBInstanceIdentifier'] == 'n8n':
-                endpoint = db_instance['Endpoint']['Address']
-                return {
-                    'DBInstanceIdentifier': 'n8n',
-                    'Endpoint': endpoint
-                }
-        return {
-            'Error': 'DB Instance n8n not found'
-        }
-    except Exception as e:
-        return {
-            'Error': str(e)
-        }
+    engine_version = response['DBInstances'][0]['EngineVersion']
+    
+    return {
+        'EngineVersion': engine_version
+    }
