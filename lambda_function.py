416caba31a72e5da8ebd428bd664d8b9b88dd922
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 18:22:34 +0530

    Remove termination protection and terminate EC2 instances

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,19 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2_client = boto3.client('ec2', region_name='us-east-1')
+    # Initialize a session using Amazon EC2
+    ec2 = boto3.client('ec2', region_name='us-east-1')
+    
     instance_ids = ['i-0d7297d85a0b55558', 'i-00120a83bfd0cd783']
     
-    # Terminate the instances
-    response = ec2_client.terminate_instances(InstanceIds=instance_ids)
-    return response
+    # First, disable termination protection
+    for instance_id in instance_ids:
+        ec2.modify_instance_attribute(InstanceId=instance_id, DisableApiTermination={'Value': False})
+
+    # Then terminate the instances
+    ec2.terminate_instances(InstanceIds=instance_ids)
+
+    return {
+        'statusCode': 200,
+        'body': f'Terminated instances: {instance_ids}'
+    }
