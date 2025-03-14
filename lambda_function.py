04ba3b266222565b1a32c9b21345b2ee264b4d2e
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 11:04:38 +0530

    Disable termination protection and terminate EC2 instance i-04de0ef02ade9172c

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,19 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2_client = boto3.client('ec2', region_name='us-east-1')
+    ec2 = boto3.client('ec2', region_name='us-east-1')
+    instance_id = 'i-04de0ef02ade9172c'
 
     # Disable termination protection
-    ec2_client.modify_instance_attribute(
-        InstanceId='i-04de0ef02ade9172c',
-        DisableApiTermination={
-            'Value': False
-        }
+    ec2.modify_instance_attribute(
+        InstanceId=instance_id,
+        DisableApiTermination={'Value': False}
     )
 
-    # Terminate the EC2 instance
-    ec2_client.terminate_instances(
-        InstanceIds=['i-04de0ef02ade9172c']
-    )
+    # Terminate the instance
+    ec2.terminate_instances(InstanceIds=[instance_id])
 
-    return 'Termination protection disabled and instance terminated'
+    return {
+        'statusCode': 200,
+        'body': f'Instance {instance_id} termination initiated.'
+    }
