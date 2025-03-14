6d7bda3ccd81cf18cb60ced94756ae74f39c2492
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 11:21:59 +0530

    Remove termination protection and terminate instances i-0d1cbea896c41b31c and i-040482c178d1bb236

--- lambda_function.py
+++ lambda_function.py
@@ -2,40 +2,14 @@ import boto3
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name='us-east-1')
-    
-    # Launch a new EC2 instance
-    instance = ec2.run_instances(
-        ImageId='ami-08b5b3a93ed654d19',
-        InstanceType='t3.micro',
-        MinCount=1,
-        MaxCount=1,
-        TagSpecifications=[
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': 'n8n-instance'
-                    }
-                ]
-            }
-        ],
-        MetadataOptions={
-            'HttpEndpoint': 'enabled',
-            'HttpTokens': 'optional'  # IMDSv2 set to Optional
-        }
-    )
-    instance_id = instance['Instances'][0]['InstanceId']
-    
-    # Enable termination protection
-    ec2.modify_instance_attribute(
-        InstanceId=instance_id,
-        DisableApiTermination={
-            'Value': True
-        }
-    )
 
-    return {
-        'statusCode': 200,
-        'body': f'Instance {instance_id} created with termination protection and IMDSv2 set to Optional'
-    }
+    instance_ids = ['i-0d1cbea896c41b31c', 'i-040482c178d1bb236']
+
+    # Disable termination protection
+    ec2.modify_instance_attribute(InstanceId='i-0d1cbea896c41b31c', DisableApiTermination={'Value': False})
+    ec2.modify_instance_attribute(InstanceId='i-040482c178d1bb236', DisableApiTermination={'Value': False})
+
+    # Terminate instances
+    ec2.terminate_instances(InstanceIds=instance_ids)
+
+    return "Instances terminated successfully"
