ed3c66a099a2e75c423e5348f5a63b5f5f6a9637
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 10:16:34 +0530

    Add Lambda function to create an EC2 instance with default AMI and instance type

--- lambda_function.py
+++ lambda_function.py
@@ -1,41 +1,13 @@
 import boto3
-from botocore.exceptions import ClientError
-import json
-
-# Initialize a session using Amazon RDS
-client = boto3.client('rds', region_name='us-east-1')
 
 def lambda_handler(event, context):
-    try:
-        # Get RDS instances
-        response = client.describe_db_instances()
-        
-        # Extracting the DB instances details
-        db_instances = response['DBInstances']
-        
-        # Formatting the output
-        instances_info = []
-        for instance in db_instances:
-            instance_info = {
-                'DBInstanceIdentifier': instance['DBInstanceIdentifier'],
-                'DBInstanceClass': instance['DBInstanceClass'],
-                'Engine': instance['Engine'],
-                'DBInstanceStatus': instance['DBInstanceStatus'],
-                'MasterUsername': instance['MasterUsername'],
-                'Endpoint': instance['Endpoint']['Address'],
-                'AllocatedStorage': instance['AllocatedStorage'],
-                'InstanceCreateTime': str(instance['InstanceCreateTime']),
-                'AvailabilityZone': instance['AvailabilityZone']
-            }
-            instances_info.append(instance_info)
-        
-        # Return the JSON serialized list of instances
-        return {
-            'statusCode': 200,
-            'body': json.dumps(instances_info)
-        }
-    except ClientError as e:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': str(e)})
-        }
+    ec2_client = boto3.client('ec2', region_name='us-east-1')
+
+    response = ec2_client.run_instances(
+        ImageId='ami-08b5b3a93ed654d19',
+        InstanceType='t3.micro',
+        MinCount=1,
+        MaxCount=1
+    )
+
+    return response['Instances'][0]['InstanceId']
