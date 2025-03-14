1b3c1b194d9254d6c8ab01459af6b1cc138bd832
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 11:07:54 +0530

    Create EC2 instance with termination protection, IMDSv2 optional, and tagging

--- lambda_function.py
+++ lambda_function.py
@@ -2,18 +2,40 @@ import boto3
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name='us-east-1')
-    instance_id = 'i-04de0ef02ade9172c'
-
-    # Disable termination protection
+    
+    # Launch a new EC2 instance
+    instance = ec2.run_instances(
+        ImageId='ami-08b5b3a93ed654d19',
+        InstanceType='t3.micro',
+        MinCount=1,
+        MaxCount=1,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': 'n8n-instance'
+                    }
+                ]
+            }
+        ],
+        MetadataOptions={
+            'HttpEndpoint': 'enabled',
+            'HttpTokens': 'optional'  # IMDSv2 set to Optional
+        }
+    )
+    instance_id = instance['Instances'][0]['InstanceId']
+    
+    # Enable termination protection
     ec2.modify_instance_attribute(
         InstanceId=instance_id,
-        DisableApiTermination={'Value': False}
+        DisableApiTermination={
+            'Value': True
+        }
     )
-
-    # Terminate the instance
-    ec2.terminate_instances(InstanceIds=[instance_id])
 
     return {
         'statusCode': 200,
-        'body': f'Instance {instance_id} termination initiated.'
+        'body': f'Instance {instance_id} created with termination protection and IMDSv2 set to Optional'
     }
