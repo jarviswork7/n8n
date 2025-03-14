48acc7e2dd5de41cb87bf1cbc6f103ae37ea6a3d
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 16:48:07 +0530

    Create EC2 instance using Lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,14 +1,14 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name='us-east-1')
+    ec2_client = boto3.client('ec2', region_name='us-east-1')
     
-    instance = ec2.run_instances(
+    response = ec2_client.run_instances(
         ImageId='ami-08b5b3a93ed654d19',
         InstanceType='t3.micro',
         MinCount=1,
         MaxCount=1
     )
     
-    instance_id = instance['Instances'][0]['InstanceId']
+    instance_id = response['Instances'][0]['InstanceId']
     return {'InstanceId': instance_id}
