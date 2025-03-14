30648cd2a921e2b93bea44ec4a3752a2f6639afb
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 18:19:51 +0530

    Fix issue with setting EC2 IMDSv2 to optional for specific instances

--- lambda_function.py
+++ lambda_function.py
@@ -3,18 +3,21 @@ from botocore.exceptions import ClientError
 
 region = 'us-east-1'
 
-# Specify the Instance IDs for which we want to set IMDSv2 to optional
+# Modify instance metadata options for a list of instance IDs
 instance_ids = ['i-0d7297d85a0b55558', 'i-00120a83bfd0cd783']
 
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
-    try:
-        # Modify the instance metadata options
-        response = ec2.modify_instance_metadata_options(
-            InstanceId=event['instance_id'],
-            HttpTokens='optional'  # Set IMDSv2 to optional
-        )
-        return response
-    except ClientError as e:
-        return {'Error': str(e)}
+    results = {}
+    for instance_id in instance_ids:
+        try:
+            # Modify the instance metadata options to make IMDSv2 optional
+            response = ec2.modify_instance_metadata_options(
+                InstanceId=instance_id,
+                HttpTokens='optional'  # Set IMDSv2 to optional
+            )
+            results[instance_id] = response
+        except ClientError as e:
+            results[instance_id] = {'Error': str(e)}
+    return results
