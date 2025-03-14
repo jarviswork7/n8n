16554888bba22aeef77f0f38f44a3a4dfb6a29fb
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 16:49:47 +0530

    Add Lambda function to retrieve EC2 storage details

--- lambda_function.py
+++ lambda_function.py
@@ -2,13 +2,22 @@ import boto3
 
 def lambda_handler(event, context):
     ec2_client = boto3.client('ec2', region_name='us-east-1')
+    instance_id = 'i-0dd5489415c629706'
     
-    response = ec2_client.run_instances(
-        ImageId='ami-08b5b3a93ed654d19',
-        InstanceType='t3.micro',
-        MinCount=1,
-        MaxCount=1
-    )
+    # Describe the instance to get storage details
+    reservations = ec2_client.describe_instances(InstanceIds=[instance_id]).get('Reservations', [])
     
-    instance_id = response['Instances'][0]['InstanceId']
-    return {'InstanceId': instance_id}
+    storage_details = []
+    for reservation in reservations:
+        for instance in reservation.get('Instances', []):
+            block_mappings = instance.get('BlockDeviceMappings', [])
+            for block in block_mappings:
+                volume_id = block.get('Ebs', {}).get('VolumeId')
+                if volume_id:
+                    volume = ec2_client.describe_volumes(VolumeIds=[volume_id]).get('Volumes', [])[0]
+                    storage_details.append({
+                        'VolumeId': volume_id,
+                        'Size': volume.get('Size'),
+                        'Type': volume.get('VolumeType'),
+                    })
+    return storage_details
