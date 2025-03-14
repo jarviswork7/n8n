66e9f41894c7fed54d7db9da5c21eaeac7500b2a
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 16:52:16 +0530

    Added code to list running RDS instances in us-east-1

--- lambda_function.py
+++ lambda_function.py
@@ -1,23 +1,18 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2_client = boto3.client('ec2', region_name='us-east-1')
-    instance_id = 'i-0dd5489415c629706'
-    
-    # Describe the instance to get storage details
-    reservations = ec2_client.describe_instances(InstanceIds=[instance_id]).get('Reservations', [])
-    
-    storage_details = []
-    for reservation in reservations:
-        for instance in reservation.get('Instances', []):
-            block_mappings = instance.get('BlockDeviceMappings', [])
-            for block in block_mappings:
-                volume_id = block.get('Ebs', {}).get('VolumeId')
-                if volume_id:
-                    volume = ec2_client.describe_volumes(VolumeIds=[volume_id]).get('Volumes', [])[0]
-                    storage_details.append({
-                        'VolumeId': volume_id,
-                        'Size': volume.get('Size'),
-                        'Type': volume.get('VolumeType'),
-                    })
-    return storage_details
+    rds_client = boto3.client('rds', region_name='us-east-1')
+    instances = rds_client.describe_db_instances()
+    rds_details = []
+    for instance in instances['DBInstances']:
+        instance_info = {
+            'DBInstanceIdentifier': instance['DBInstanceIdentifier'],
+            'DBInstanceClass': instance['DBInstanceClass'],
+            'Engine': instance['Engine'],
+            'DBInstanceStatus': instance['DBInstanceStatus'],
+            'AllocatedStorage': instance['AllocatedStorage'],
+            'StorageType': instance['StorageType'],
+            'MultiAZ': instance['MultiAZ']
+        }
+        rds_details.append(instance_info)
+    return rds_details
