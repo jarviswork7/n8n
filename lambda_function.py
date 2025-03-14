be870ad7635e1832dcfbc1445494eadc778db927
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 10:21:09 +0530

    Add tag 'Name' with value 'n8n-instance' to EC2 instance i-08b5ba99795323b9a

--- lambda_function.py
+++ lambda_function.py
@@ -2,5 +2,12 @@ import boto3
 
 def lambda_handler(event, context):
     ec2_client = boto3.client('ec2', region_name='us-east-1')
-    ec2_client.create_tags(Resources=['i-08b5ba99795323b9a'], Tags=[{'Key': 'name', 'Value': 'n8n-instance'}])
-    return 'Instance tagged successfully'
+    
+    try:
+        response = ec2_client.create_tags(
+            Resources=['i-08b5ba99795323b9a'],
+            Tags=[{'Key': 'Name', 'Value': 'n8n-instance'}]
+        )
+        return {'status': 'Tagging successful', 'response': response}
+    except Exception as e:
+        return {'status': 'Tagging failed', 'error': str(e)}
