17167053384fd6f4a630cd74fad24d8667ddc746
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 15:54:00 +0530

    Add code to list S3 buckets using Python with boto3

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,7 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name='us-east-1')
-
-    instance_ids = ['i-0d1cbea896c41b31c', 'i-040482c178d1bb236']
-
-    # Disable termination protection
-    ec2.modify_instance_attribute(InstanceId='i-0d1cbea896c41b31c', DisableApiTermination={'Value': False})
-    ec2.modify_instance_attribute(InstanceId='i-040482c178d1bb236', DisableApiTermination={'Value': False})
-
-    # Terminate instances
-    ec2.terminate_instances(InstanceIds=instance_ids)
-
-    return "Instances terminated successfully"
+    s3 = boto3.client('s3', region_name='us-east-1')
+    response = s3.list_buckets()
+    buckets = [bucket['Name'] for bucket in response['Buckets']]
+    return buckets
