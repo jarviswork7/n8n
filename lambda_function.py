7431d7ceda7aab96aca0f71831dc485fbdf6abc3
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 18:21:10 +0530

    Add Lambda function to terminate EC2 instances i-0d7297d85a0b55558 and i-00120a83bfd0cd783

--- lambda_function.py
+++ lambda_function.py
@@ -1,23 +1,9 @@
 import boto3
-from botocore.exceptions import ClientError
-
-region = 'us-east-1'
-
-# Modify instance metadata options for a list of instance IDs
-instance_ids = ['i-0d7297d85a0b55558', 'i-00120a83bfd0cd783']
-
-ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
-    results = {}
-    for instance_id in instance_ids:
-        try:
-            # Modify the instance metadata options to make IMDSv2 optional
-            response = ec2.modify_instance_metadata_options(
-                InstanceId=instance_id,
-                HttpTokens='optional'  # Set IMDSv2 to optional
-            )
-            results[instance_id] = response
-        except ClientError as e:
-            results[instance_id] = {'Error': str(e)}
-    return results
+    ec2_client = boto3.client('ec2', region_name='us-east-1')
+    instance_ids = ['i-0d7297d85a0b55558', 'i-00120a83bfd0cd783']
+    
+    # Terminate the instances
+    response = ec2_client.terminate_instances(InstanceIds=instance_ids)
+    return response
