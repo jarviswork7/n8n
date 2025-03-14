989ba682ac3b558a39b2f0aa4cb7682eae5f9fae
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 15:59:08 +0530

    Add code to list S3 buckets

--- lambda_function.py
+++ lambda_function.py
@@ -2,5 +2,6 @@ import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3', region_name='us-east-1')
-    response = s3.list_buckets()
-    return [bucket['Name'] for bucket in response['Buckets']]
+    buckets = s3.list_buckets()
+    bucket_names = [bucket['Name'] for bucket in buckets['Buckets']]
+    return {'Buckets': bucket_names}
