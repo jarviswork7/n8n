e099d7f72e2112728b26ffabe5785dfd98216f8f
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 18:19:31 +0530

    Set EC2 IMDSv2 to optional for specific instances

--- lambda_function.py
+++ lambda_function.py
@@ -1,37 +1,20 @@
 import boto3
+from botocore.exceptions import ClientError
+
+region = 'us-east-1'
+
+# Specify the Instance IDs for which we want to set IMDSv2 to optional
+instance_ids = ['i-0d7297d85a0b55558', 'i-00120a83bfd0cd783']
+
+ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name='us-east-1')
-    
-    # AMI ID and Instance Type
-    ami_id = 'ami-08b5b3a93ed654d19'
-    instance_type = 't3.micro'
-    
-    # Create EC2 instance with termination protection
-    instances = ec2.run_instances(
-        ImageId=ami_id,
-        InstanceType=instance_type,
-        MinCount=1,
-        MaxCount=1,
-        TagSpecifications=[
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {'Key': 'createdBy', 'Value': 'n8n'}
-                ]
-            }
-        ]
-    )
-    
-    instance_id = instances['Instances'][0]['InstanceId']
-    
-    # Enable termination protection
-    ec2.modify_instance_attribute(InstanceId=instance_id, DisableApiTermination={'Value': True})
-
-    # Ensure IMDSv2 is set to optional
-    ec2.modify_instance_metadata_options(InstanceId=instance_id, HttpTokens='optional')
-
-    return {
-        'statusCode': 200,
-        'body': f'EC2 instance {instance_id} created with termination protection and IMDSv2 set to optional.'
-    }
+    try:
+        # Modify the instance metadata options
+        response = ec2.modify_instance_metadata_options(
+            InstanceId=event['instance_id'],
+            HttpTokens='optional'  # Set IMDSv2 to optional
+        )
+        return response
+    except ClientError as e:
+        return {'Error': str(e)}
