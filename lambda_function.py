450cb612b129cff7d4d83a27e2f97b764099041e
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-14 18:33:29 +0530

    Create EC2 instance with best practices

--- lambda_function.py
+++ lambda_function.py
@@ -6,23 +6,30 @@ def lambda_handler(event, context):
     # Create a new EC2 instance
     instances = ec2.create_instances(
         ImageId='ami-08b5b3a93ed654d19',
+        InstanceType='t3.micro',
         MinCount=1,
         MaxCount=1,
-        InstanceType='t3.micro',
         TagSpecifications=[
             {
                 'ResourceType': 'instance',
                 'Tags': [
-                    {'Key': 'createdBy', 'Value': 'Daniel'},
-                    {'Key': 'Name', 'Value': 'n8n'}
+                    {'Key': 'Name', 'Value': 'n8n'},
+                    {'Key': 'createdBy', 'Value': 'Daniel'}
                 ]
             }
-        ]
+        ],
+        MetadataOptions={
+            'HttpTokens': 'required'
+        }
     )
     
-    # Enable termination protection
     instance_id = instances[0].id
-    ec2_client = boto3.client('ec2', region_name='us-east-1')
-    ec2_client.modify_instance_attribute(InstanceId=instance_id, DisableApiTermination={'Value': True})
-    
-    return {'InstanceId': instance_id}
+
+    # Enable termination protection
+    ec2_instance = ec2.Instance(instance_id)
+    ec2_instance.modify_attribute(DisableApiTermination={'Value': True})
+
+    return {
+        'statusCode': 200,
+        'body': f'EC2 Instance {instance_id} created successfully with termination protection enabled.'
+    }
