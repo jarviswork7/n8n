56ff7a6aedc95e3128b6ae69588635b0e290eed1
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-16 17:09:33 +0530

    Create Lambda function to provision EC2 instance

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,48 @@
-import json
 import boto3
 
 def lambda_handler(event, context):
+    """
+    Create an EC2 instance with best practices.
+    """
+    ec2 = boto3.resource('ec2')
+
+    # Create a new EC2 instance
+    instance = ec2.create_instances(
+        ImageId='ami-08b5b3a93ed654d19',
+        InstanceType='t3.micro',
+        MinCount=1,
+        MaxCount=1,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'createdBy',
+                        'Value': 'Daniel'
+                    },
+                    {
+                        'Key': 'Name',
+                        'Value': 'n8n'
+                    }
+                ]
+            }
+        ],
+        IamInstanceProfile={
+            'Name': 'iam-role-name'  # Replace with the actual IAM role if necessary
+        },
+        NetworkInterfaces=[{
+            'SubnetId': 'subnet-id',  # Replace with the actual subnet ID
+            'DeviceIndex': 0,
+            'AssociatePublicIpAddress': False  # Disable Public IP
+        }]
+    )
+
+    # Enable termination protection
+    instance[0].modify_attribute(
+        DisableApiTermination={'Value': True}
+    )
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Lambda function created successfully!')
+        'body': f'EC2 instance {instance[0].id} created successfully.'
     }
