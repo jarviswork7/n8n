835eb54d8530d39024405afebe7c64a228d77fdf
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-16 14:48:02 +0530

    Add Lambda function to list all Lambda functions with details

--- lambda_function.py
+++ lambda_function.py
@@ -1,35 +1,15 @@
 import boto3
+import json
 
 def lambda_handler(event, context):
-    ec2 = boto3.resource('ec2', region_name='us-east-1')
+    # Initialize a session using Amazon Lambda
+    client = boto3.client('lambda', region_name='us-east-1')
     
-    # Create a new EC2 instance
-    instances = ec2.create_instances(
-        ImageId='ami-08b5b3a93ed654d19',
-        InstanceType='t3.micro',
-        MinCount=1,
-        MaxCount=1,
-        TagSpecifications=[
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {'Key': 'Name', 'Value': 'n8n'},
-                    {'Key': 'createdBy', 'Value': 'Daniel'}
-                ]
-            }
-        ],
-        MetadataOptions={
-            'HttpTokens': 'required'
-        }
-    )
+    # List all functions
+    response = client.list_functions()
     
-    instance_id = instances[0].id
-
-    # Enable termination protection
-    ec2_instance = ec2.Instance(instance_id)
-    ec2_instance.modify_attribute(DisableApiTermination={'Value': True})
-
+    # Return the list of functions
     return {
         'statusCode': 200,
-        'body': f'EC2 Instance {instance_id} created successfully with termination protection enabled.'
+        'body': json.dumps(response)
     }
