39a0b1926cd8990f830c046cf5c451e71caddc12
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-16 17:01:38 +0530

    Created a Lambda function with specified best practices

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
-import json
+import json 
 
-def lambda_handler(event, context):
+def lambda_handler(event, context): 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from n8n Lambda function!')
