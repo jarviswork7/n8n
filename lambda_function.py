a216b9ef2f57b1606f2ee05ed37377a9e7247d37
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-16 14:54:59 +0530

    Add code to list all S3 buckets with details.

--- lambda_function.py
+++ lambda_function.py
@@ -3,19 +3,23 @@ import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
+    # Retrieve the list of S3 buckets
     response = s3.list_buckets()
-
     buckets = response['Buckets']
+    
+    # List to hold bucket details
     bucket_details = []
-
+    
+    # Get details for each bucket
     for bucket in buckets:
-        # Get creation date in a readable format
+        bucket_name = bucket['Name']
         creation_date = bucket['CreationDate'].strftime("%Y-%m-%d %H:%M:%S")
+        
         bucket_details.append({
-            'Name': bucket['Name'],
-            'CreationDate': creation_date
+            "Bucket Name": bucket_name,
+            "Creation Date": creation_date
         })
-
+    
     return {
         'statusCode': 200,
         'body': json.dumps(bucket_details)
