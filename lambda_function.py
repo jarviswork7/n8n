34256b1937fd080d95f73603a4d6af7b93686fe6
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-16 17:02:24 +0530

    Create AWS Lambda function with best practices

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,11 @@
-import json 
+import json
 
-def lambda_handler(event, context): 
+def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from n8n Lambda function!')
+        'body': json.dumps('Hello from n8n Lambda function!'),
+        'tags': {
+            'createdBy': 'Daniel',
+            'Name': 'n8n'
+        }
     }
