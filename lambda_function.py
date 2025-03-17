bd90b0a2f0ccc4093ff51078db30b31da3134328
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-17 20:10:15 +0530

    Simplified EC2 creation with best practices and rules

--- lambda_function.py
+++ lambda_function.py
@@ -2,13 +2,21 @@ import boto3
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name='us-east-1')
-    try:
-        instance = ec2.run_instances(
-            ImageId='ami-08b5b3a93ed654d19',
-            InstanceType='t3.micro',
-            MinCount=1,
-            MaxCount=1
-        )
-        return {'InstanceId': instance['Instances'][0]['InstanceId']}
-    except Exception as e:
-        return {'Status': 'Error', 'Message': str(e)}
+    instance_params = {
+        'ImageId': 'ami-08b5b3a93ed654d19',
+        'InstanceType': 't3.micro',
+        'MinCount': 1,
+        'MaxCount': 1,
+        'TagSpecifications': [
+            { 'ResourceType': 'instance', 'Tags': [{ 'Key': 'createdBy', 'Value': 'Daniel' }, { 'Key': 'Name', 'Value': 'n8n' }]}],
+        'MetadataOptions': { 'HttpTokens': 'required'},
+        'DisableApiTermination': True,
+        'NetworkInterfaces': [
+            {
+                'AssociatePublicIpAddress': False,
+                'DeviceIndex': 0
+            }
+        ]
+    }
+    instance = ec2.run_instances(**instance_params)
+    return {'InstanceId': instance['Instances'][0]['InstanceId']}
