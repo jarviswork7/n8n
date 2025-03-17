fb358486c370793c3fc88c935799859d0059bfd0
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-17 20:07:37 +0530

    Add Lambda function to create an EC2 instance with best practices.

--- lambda_function.py
+++ lambda_function.py
@@ -1,58 +1,36 @@
 import boto3
-import logging
 
 def lambda_handler(event, context):
-    logging.basicConfig(level=logging.INFO)
-    ec2_client = boto3.client('ec2', region_name='us-east-1')
+    ec2 = boto3.client('ec2', region_name='us-east-1')  # Default region
+    
+    # Best practices
+    instance_params = {
+        'ImageId': 'ami-08b5b3a93ed654d19',
+        'InstanceType': 't3.micro',
+        'MinCount': 1,
+        'MaxCount': 1,
+        'TagSpecifications': [
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {'Key': 'createdBy', 'Value': 'Daniel'},
+                    {'Key': 'Name', 'Value': 'n8n'}
+                ]
+            }
+        ],
+        'MetadataOptions': {
+            'HttpEndpoint': 'enable',
+            'HttpProtocolIpv6': 'disabled',
+            'HttpPutResponseHopLimit': 2,
+            'HttpTokens': 'required'  # IMDSv2
+        },
+        'DisableApiTermination': True,  # Enable termination protection
+    }
 
-    try:
-        logging.info('Attempting to launch EC2 instance...')
-        # Launch EC2 instance
-        instances = ec2_client.run_instances(
-            ImageId='ami-08b5b3a93ed654d19',  # Provided AMI ID
-            InstanceType='t3.micro',
-            MinCount=1,
-            MaxCount=1,
-            NetworkInterfaces=[{
-                'AssociatePublicIpAddress': False,  # Disable auto-assign public IP
-                'DeleteOnTermination': True,
-                'DeviceIndex': 0,
-                'SubnetId': 'subnet-xxxxxxxx',  # Ensure to select a private subnet
-            }],
-            TagSpecifications=[
-                {
-                    'ResourceType': 'instance',
-                    'Tags': [
-                        {'Key': 'createdBy', 'Value': 'Daniel'},
-                        {'Key': 'Name', 'Value': 'n8n'}
-                    ]
-                }
-            ]
-        )
-
-        instance_id = instances['Instances'][0]['InstanceId']
-        logging.info(f'Instance {instance_id} launched successfully.')
-
-        # Enable termination protection
-        ec2_client.modify_instance_attribute(
-            InstanceId=instance_id,
-            DisableApiTermination={'Value': True}
-        )
-
-        # Enforce IMDSv2
-        ec2_client.modify_instance_metadata_options(
-            InstanceId=instance_id,
-            HttpTokens='required'
-        )
-
-        return {
-            'statusCode': 200,
-            'body': f'EC2 Instance {instance_id} created successfully.'
-        }
-
-    except Exception as e:
-        logging.error(f'An error occurred: {str(e)}')
-        return {
-            'statusCode': 500,
-            'body': f'Error creating EC2 Instance: {str(e)}'
-        }
+    # Launching the EC2 instance
+    instance = ec2.run_instances(**instance_params)
+    
+    return {
+        'InstanceId': instance['Instances'][0]['InstanceId'],
+        'State': instance['Instances'][0]['State']['Name']
+    }
