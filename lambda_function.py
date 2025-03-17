ffc32922992002c9d4211e8821d0e9961cd7aecd
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-17 20:12:10 +0530

    Create EC2 instance with type t2.micro

--- lambda_function.py
+++ lambda_function.py
@@ -2,21 +2,25 @@ import boto3
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name='us-east-1')
-    instance_params = {
-        'ImageId': 'ami-08b5b3a93ed654d19',
-        'InstanceType': 't3.micro',
-        'MinCount': 1,
-        'MaxCount': 1,
-        'TagSpecifications': [
-            { 'ResourceType': 'instance', 'Tags': [{ 'Key': 'createdBy', 'Value': 'Daniel' }, { 'Key': 'Name', 'Value': 'n8n' }]}],
-        'MetadataOptions': { 'HttpTokens': 'required'},
-        'DisableApiTermination': True,
-        'NetworkInterfaces': [
-            {
-                'AssociatePublicIpAddress': False,
-                'DeviceIndex': 0
+    try:
+        instance = ec2.run_instances(
+            ImageId='ami-08b5b3a93ed654d19',
+            InstanceType='t2.micro',
+            MinCount=1,
+            MaxCount=1,
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {'Key': 'createdBy', 'Value': 'Daniel'},
+                        {'Key': 'Name', 'Value': 'n8n'}
+                    ]
+                }
+            ],
+            MetadataOptions={
+                'HttpTokens': 'required',  # IMDSv2
             }
-        ]
-    }
-    instance = ec2.run_instances(**instance_params)
-    return {'InstanceId': instance['Instances'][0]['InstanceId']}
+        )
+        return {'InstanceId': instance['Instances'][0]['InstanceId']}
+    except Exception as e:
+        return {'Error': str(e)}
