2886fe4bb8376aa1ab4d23ed6d6b5197f06bf13f
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-17 20:57:00 +0530

    Delete S3 bucket 'n8n-function'

--- lambda_function.py
+++ lambda_function.py
@@ -1,32 +1,9 @@
 import boto3
 
-s3 = boto3.client('s3')
-
 def lambda_handler(event, context):
-    # Create S3 bucket
-    bucket_name = "n8n-function"
-    s3.create_bucket(Bucket=bucket_name)
+    s3 = boto3.client('s3')
+    bucket_name = 'n8n-function'
 
-    # Enable versioning
-    s3.put_bucket_versioning(
-        Bucket=bucket_name,
-        VersioningConfiguration={
-            'Status': 'Enabled'
-        }
-    )
-
-    # Add tags to the bucket
-    s3.put_bucket_tagging(
-        Bucket=bucket_name,
-        Tagging={
-            'TagSet': [
-                {'Key': 'createdBy', 'Value': 'Daniel'},
-                {'Key': 'Name', 'Value': 'n8n'}
-            ]
-        }
-    )
-
-    return {
-        'statusCode': 200,
-        'body': 'S3 bucket created successfully with versioning and tags.'
-    }
+    # Delete the S3 bucket
+    response = s3.delete_bucket(Bucket=bucket_name)
+    return response
