b6fced12f813496124d58661b6832522b38ef18f
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-17 20:08:55 +0530

    Simplified EC2 instance creation code.

--- lambda_function.py
+++ lambda_function.py
@@ -1,36 +1,14 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name='us-east-1')  # Default region
-    
-    # Best practices
-    instance_params = {
-        'ImageId': 'ami-08b5b3a93ed654d19',
-        'InstanceType': 't3.micro',
-        'MinCount': 1,
-        'MaxCount': 1,
-        'TagSpecifications': [
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {'Key': 'createdBy', 'Value': 'Daniel'},
-                    {'Key': 'Name', 'Value': 'n8n'}
-                ]
-            }
-        ],
-        'MetadataOptions': {
-            'HttpEndpoint': 'enable',
-            'HttpProtocolIpv6': 'disabled',
-            'HttpPutResponseHopLimit': 2,
-            'HttpTokens': 'required'  # IMDSv2
-        },
-        'DisableApiTermination': True,  # Enable termination protection
-    }
-
-    # Launching the EC2 instance
-    instance = ec2.run_instances(**instance_params)
-    
-    return {
-        'InstanceId': instance['Instances'][0]['InstanceId'],
-        'State': instance['Instances'][0]['State']['Name']
-    }
+    ec2 = boto3.client('ec2', region_name='us-east-1')
+    try:
+        instance = ec2.run_instances(
+            ImageId='ami-08b5b3a93ed654d19',
+            InstanceType='t3.micro',
+            MinCount=1,
+            MaxCount=1
+        )
+        return {'InstanceId': instance['Instances'][0]['InstanceId']}
+    except Exception as e:
+        return {'Status': 'Error', 'Message': str(e)}
