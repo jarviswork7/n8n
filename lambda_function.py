13b34520c5e59780944a946fec2e87735975d032
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-18 14:15:33 +0530

    Create EC2 instance with t3.micro type

--- lambda_function.py
+++ lambda_function.py
@@ -3,21 +3,13 @@ import boto3
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name='us-east-1')
     
-    # Describe instances to get all instance IDs
-    instances = ec2.describe_instances()
-    instance_ids = []
-    for reservation in instances['Reservations']:
-        for instance in reservation['Instances']:
-            instance_ids.append(instance['InstanceId'])
-
-    # Remove termination protection and terminate instances
-    for instance_id in instance_ids:
-        try:
-            # Modify instance attribute to disable termination protection
-            ec2.modify_instance_attribute(InstanceId=instance_id, DisableApiTermination={'Value': False})
-            
-            # Terminate the instance
-            ec2.terminate_instances(InstanceIds=[instance_id])
-            print(f'Terminated instance: {instance_id}')
-        except Exception as e:
-            print(f'Error terminating instance {instance_id}: {str(e)}')
+    # Launch a new EC2 instance
+    instances = ec2.run_instances(
+        ImageId='ami-08b5b3a93ed654d19',
+        InstanceType='t3.micro',
+        MinCount=1,
+        MaxCount=1
+    )
+    
+    instance_id = instances['Instances'][0]['InstanceId']
+    return {'InstanceId': instance_id}
