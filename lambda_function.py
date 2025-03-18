667da9ffc410148feb58f7b9085b49381b75c7c7
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-18 13:49:00 +0530

    Create EC2 instance with t2.micro and specified best practices

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,26 @@
 import boto3
-
 def lambda_handler(event, context):
-    ec2 = boto3.resource('ec2', region_name='us-east-1')
-    instance = ec2.create_instances(
+    ec2_client = boto3.client('ec2', region_name='us-east-1')
+    
+    response = ec2_client.run_instances(
         ImageId='ami-08b5b3a93ed654d19',
-        InstanceType='t3.micro',
+        InstanceType='t2.micro',
         MinCount=1,
-        MaxCount=1
+        MaxCount=1,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {'Key': 'createdBy', 'Value': 'Daniel'},
+                    {'Key': 'Name', 'Value': 'n8n'}
+                ]
+            }
+        ],
+        InstanceInitiatedShutdownBehavior='stop',
+        MetadataOptions={
+            'HttpEndpoint': 'enabled',
+            'HttpTokens': 'required',
+        }
     )
-    instance_id = instance[0].id
+    instance_id = response['Instances'][0]['InstanceId']
     return {'instance_id': instance_id}
