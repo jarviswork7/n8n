57ecd98de0056222520d0ad0e415b77ebbe4ee55
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-18 14:03:07 +0530

    Create an EC2 server with t3.micro instance type including best practices and tagging.

--- lambda_function.py
+++ lambda_function.py
@@ -1,32 +1,30 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name='us-east-1')
-
-    # Describe instances to get the IDs
-    instances = ec2.describe_instances()
-
-    instance_ids = []
-    for reservation in instances['Reservations']:
-        for instance in reservation['Instances']:
-            instance_ids.append(instance['InstanceId'])
-
-    # Remove termination protection and terminate instances
-    for instance_id in instance_ids:
-        try:
-            # Remove termination protection
-            ec2.modify_instance_attribute(
-                InstanceId=instance_id,
-                DisableApiTermination={'Value': False}
-            )
-
-            # Terminate instance
-            ec2.terminate_instances(InstanceIds=[instance_id])
-
-        except Exception as e:
-            print(f"Error for instance {instance_id}: {e}")
-
-    return {
-        'statusCode': 200,
-        'body': f'Terminated instances: {instance_ids}'
-    }
+    ec2 = boto3.resource('ec2', region_name='us-east-1')
+    
+    # Create a new EC2 instance
+    instances = ec2.create_instances(
+        ImageId='ami-08b5b3a93ed654d19',
+        InstanceType='t3.micro',
+        MinCount=1,
+        MaxCount=1,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {'Key': 'createdBy', 'Value': 'Daniel'},
+                    {'Key': 'Name', 'Value': 'n8n'}
+                ]
+            }
+        ],
+        InstanceInitiatedShutdownBehavior='terminate',
+        MetadataOptions={
+            'HttpTokens': 'required',
+        },
+        DisableApiTermination=False
+    )
+    
+    instance_id = instances[0].id
+    
+    return {'InstanceId': instance_id}
