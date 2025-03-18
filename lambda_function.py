720c86b3fd51497166a2004cc2b59e011340d521
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-18 14:19:55 +0530

    Create EC2 instance with t3.micro and best practices

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,35 @@
 import boto3
 
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name='us-east-1')
-    
-    # Launch a new EC2 instance
-    instances = ec2.run_instances(
-        ImageId='ami-08b5b3a93ed654d19',
-        InstanceType='t3.micro',
-        MinCount=1,
-        MaxCount=1
-    )
-    
-    instance_id = instances['Instances'][0]['InstanceId']
-    return {'InstanceId': instance_id}
+# Initialize a session using Amazon EC2
+session = boto3.Session(region_name="us-east-1")
+ec2 = session.resource('ec2')
+
+# Create a new EC2 instance
+instances = ec2.create_instances(
+    ImageId='ami-08b5b3a93ed654d19',
+    InstanceType='t3.micro',
+    MaxCount=1,
+    MinCount=1,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'createdBy', 'Value': 'Daniel'},
+                {'Key': 'Name', 'Value': 'n8n'}
+            ]
+        }
+    ],
+    InstanceInitiatedShutdownBehavior='terminate',
+    Monitoring={'Enabled': False},
+    MetadataOptions={
+        'HttpTokens': 'required',
+        'HttpEndpoint': 'enabled'
+    }
+)
+
+# Enabling termination protection
+for instance in instances:
+    instance.modify_attribute(DisableApiTermination={'Value': True})
+
+instance_id = instances[0].id
+print(f'Created EC2 instance with ID: {instance_id}')
