b0aaa4f0392b848548c37e68d9097329cbe434f8
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-18 13:03:50 +0530

    Create EC2 instance and add its ID to Excel sheet

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,38 @@
 import boto3
+import openpyxl
 
+# Initialize the EC2 client
+client = boto3.client('ec2', region_name='us-east-1')
+
+# Lambda handler
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    bucket_name = 'n8n-function'
+    # Create EC2 instance
+    instance = client.run_instances(
+        ImageId='ami-08b5b3a93ed654d19',
+        InstanceType='t3.micro',
+        MaxCount=1,
+        MinCount=1,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {'Key': 'createdBy', 'Value': 'Daniel'},
+                    {'Key': 'Name', 'Value': 'n8n'}
+                ]
+            }
+        ]
+    )
 
-    # Delete the S3 bucket
-    response = s3.delete_bucket(Bucket=bucket_name)
-    return response
+    instance_id = instance['Instances'][0]['InstanceId']
+
+    # Load the excel
+    path = '/tmp/AWS-Resources.xlsx'
+    workbook = openpyxl.load_workbook(path)
+    sheet = workbook.active
+    
+    # Add instance ID to Excel
+    sheet.append([instance_id])
+
+    workbook.save(path)
+    
+    return {'InstanceId': instance_id}
