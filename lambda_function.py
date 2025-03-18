e21ca8925c35af88816be01f1e9cb9b0e4b3f7d7
jarviswork7 <jarviscopilot7@gmail.com>
2025-03-18 14:00:41 +0530

    Fix error and handle exceptions for terminating EC2 instances

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,32 @@
 import boto3
 
-ec2 = boto3.client('ec2', region_name='us-east-1')
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name='us-east-1')
 
-# Describe instances to get the IDs
-instances = ec2.describe_instances()
+    # Describe instances to get the IDs
+    instances = ec2.describe_instances()
 
-instance_ids = []
-for reservation in instances['Reservations']:
-    for instance in reservation['Instances']:
-        instance_ids.append(instance['InstanceId'])
+    instance_ids = []
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_ids.append(instance['InstanceId'])
 
-# Remove termination protection
-for instance_id in instance_ids:
-    ec2.modify_instance_attribute(
-        InstanceId=instance_id,
-        DisableApiTermination={'Value': False}
-    )
+    # Remove termination protection and terminate instances
+    for instance_id in instance_ids:
+        try:
+            # Remove termination protection
+            ec2.modify_instance_attribute(
+                InstanceId=instance_id,
+                DisableApiTermination={'Value': False}
+            )
 
-# Terminate instances
-if instance_ids:
-    ec2.terminate_instances(InstanceIds=instance_ids)
+            # Terminate instance
+            ec2.terminate_instances(InstanceIds=[instance_id])
 
-print(f"Terminated instances: {instance_ids}")
+        except Exception as e:
+            print(f"Error for instance {instance_id}: {e}")
+
+    return {
+        'statusCode': 200,
+        'body': f'Terminated instances: {instance_ids}'
+    }
