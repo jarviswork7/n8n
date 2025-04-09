a2867c62598c231e52213eca34de5d327bff656f
jarviswork7 <jarviscopilot7@gmail.com>
2025-04-09 14:40:28 +0530

    Fix persistent error in Lambda function to create EC2 instance

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import boto3
 def lambda_handler(event, context):
     ec2 = boto3.resource('ec2', region_name='us-east-1')
     
-    # specify the AMI and instance type with necessary configurations
+    # Create EC2 instance with necessary configurations
     instances = ec2.create_instances(
         ImageId='ami-08b5b3a93ed654d19',
         InstanceType='t3.micro',
@@ -22,14 +22,14 @@ def lambda_handler(event, context):
             {
                 'AssociatePublicIpAddress': False,
                 'DeviceIndex': 0,
-                'Groups': [], # Assume defaults – must specify security group IDs if required
-                'SubnetId': 'subnet-0bb1c79de3EXAMPLE' # Replace with a private subnet ID
+                'SubnetId': 'subnet-0bb1c79de3EXAMPLE'
             }
         ]
     )
     
-    # Enable termination protection
     instance_id = instances[0].id
+    
+    # Enable termination protection
     ec2.Instance(instance_id).modify_attribute(DisableApiTermination={'Value': True})
     
     # Set instance metadata options to require IMDSv2
